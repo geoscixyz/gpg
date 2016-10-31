605adb53184ea9f8249f8f4ca8ce72e02eb61a47
dfournier <fourndo@gmail.com>
2016-10-31 12:12:34 -0700

    Update conf.py

--- conf.py
+++ conf.py
@@ -100,7 +100,8 @@ linkcheck_ignore = [
   'http://www.sgl.com/technicalpapers/EAGEabst-final-TV.pdf',
   'http://geogratis.gc.ca/site/eng/extraction',
   'http://www.ngdc.noaa.gov/*',
-  'http://www.eoas.ubc.ca/courses/eosc350/content/methods/meth_4/assets/gravgrad-figs.pdf',
+  'http://www.eoas.ubc.ca/courses/eosc350/content/*',
+  'http://www.eos.ubc.ca/courses/eosc350/content/*',
 ]
 linkcheck_retries = 3
 linkcheck_timeout = 500
